1d56d371c25704ab234b83faa507f11a6cea9cb9
Duy <alvisstar1991@gmail.com>
2015-06-19 16:39:48 +0700

    xong sinh wave

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour {
 	public List<GameObject> characterPrefabs = null;
 	public TouchController	ctrl;
 	public GUISkin	guiSkin;
+	int maxWave;
+	public List<BotControler> botScripts ;
 	// Use this for initialization
 
 	void Start () {
@@ -16,11 +18,27 @@ public class GameManager : MonoBehaviour {
 		myChar.GetComponent<PlayerControler>().Init (new Vector3 (-1, 0, 0), true);
 		camera.GetComponent<CameraControler> ().targetObject = myChar;
 		// 6
-		GameObject opponentChar = (Instantiate(characterPrefabs[1], new Vector3 (-1, 0, 0), Quaternion.identity) as GameObject);
-		BotControler opponentScript = opponentChar.GetComponent<BotControler>();
-		opponentScript.Init(new Vector3 (-3, 0, 0), false);
+		Init ();
 	}
-	
+
+	void Init()
+	{
+		maxWave = 5;
+	}
+	void SpawnWave()
+	{
+
+		//int n = Random (5, 6);
+		for (int i =0; i<5; i++) {
+			GameObject opponentChar = (Instantiate(characterPrefabs[1], new Vector3 (-1, 0, 0), Quaternion.identity) as GameObject);
+			BotControler opponentScript = opponentChar.GetComponent<BotControler>();
+			float x = Random.Range(-10.0f,10.0f);
+			float z =Random.Range(-10.0f,10.0f);
+			opponentScript.Init(new Vector3 (x, 0, z), false);
+			botScripts.Add(opponentScript);
+		}
+		maxWave--;
+	}
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (this.ctrl){	
@@ -43,7 +61,18 @@ public class GameManager : MonoBehaviour {
 		}
 	}
 	void Update () {
+		if (maxWave >= 0 && botScripts.Count==0) {
 		
+			SpawnWave();
+		}
+		for (int i =0; i< botScripts.Count; i++) {
+			if(botScripts[i].isDie ==true)
+			{
+				Destroy(botScripts[i].gameObject);
+				botScripts.RemoveAt(i);
+
+			}
+		}
 	}
 	void OnGUI()
 	{
@@ -51,6 +80,7 @@ public class GameManager : MonoBehaviour {
 		if (this.ctrl != null)
 			this.ctrl.DrawControllerGUI();
 	}
+
 
 	public void HandleFinish(int id)
 	{
