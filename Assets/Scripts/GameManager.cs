f19948473750eb5b2c1401663cd68a1c5f11f8da
vutuandung <vutuandung1991@gmail.com>
2015-06-19 16:12:41 +0700

    remove redundant code and refactor code.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,8 +10,8 @@ public class GameManager : MonoBehaviour {
 	public TouchController	ctrl;
 	public GUISkin	guiSkin;
 	// Use this for initialization
-	void Start () {
 
+	void Start () {
 		myChar = Instantiate(characterPrefabs[0], new Vector3 (-1, 0, 0), Quaternion.identity) as GameObject;
 		myChar.GetComponent<PlayerControler>().Init (new Vector3 (-1, 0, 0), true);
 		camera.GetComponent<CameraControler> ().targetObject = myChar;
@@ -19,57 +19,39 @@ public class GameManager : MonoBehaviour {
 		GameObject opponentChar = (Instantiate(characterPrefabs[1], new Vector3 (-1, 0, 0), Quaternion.identity) as GameObject);
 		BotControler opponentScript = opponentChar.GetComponent<BotControler>();
 		opponentScript.Init(new Vector3 (-3, 0, 0), false);
-		
-	
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (this.ctrl)
-		{	
-			// Get stick and zone references by IDs...
-			
+		if (this.ctrl){	
+			// Get stick and zone references by IDs...			
 			TouchStick 
 				walkStick = this.ctrl.GetStick(0);
 			
-			if (walkStick.Pressed())
-			{	
-				
+			if (walkStick.Pressed()){
+				float playerSpeed = myChar.GetComponent<PlayerControler>().speed;
 				myChar.GetComponent<PlayerControler>().Move (walkStick.GetVec3d(true, 0));
-				myChar.GetComponent<Rigidbody> ().velocity = walkStick.GetVec3d(true, 0) * 0.1f * 100;
-				
-			}
-			
+				myChar.GetComponent<Rigidbody> ().velocity = walkStick.GetVec3d(true, 0) * playerSpeed * 100;				
+			}			
 			// Stop when stick is released...
 			
-			else
-			{
+			else {
 				
 			}
-			
-			
 			// Shoot when right stick is pressed...
-			
-			
-		}
 
+		}
 	}
 	void Update () {
-
-
-
-
 		
 	}
 	void OnGUI()
 	{
-		// Manually draw the controller...
-		
+		// Manually draw the controller...		
 		if (this.ctrl != null)
 			this.ctrl.DrawControllerGUI();
-		
-	
 	}
+
 	public void HandleFinish(int id)
 	{
 		Debug.Log(id + " win, congratulation");
--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -34,35 +34,15 @@ public class PlayerControler : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-		// read inputs
 		float h = Input.GetAxis("Horizontal");
 		float v = Input.GetAxis("Vertical");
-		bool crouch = Input.GetKey(KeyCode.C);
-		
-		// calculate move direction to pass to character
-//		if (m_Cam != null)
-//		{
-//			// calculate camera relative direction to move:
-//			m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-//			m_Move = v*m_CamForward + h*m_Cam.right;
-//		}
-//		else
-//		{
-			// we use world-relative directions in the case of no main camera
-			m_Move = v*Vector3.forward + h*Vector3.right;
-//		}
-//		#if !MOBILE_INPUT
-//		// walk speed multiplier
-//		if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
-//		#endif
-		
-		// pass all parameters to the character control script
+		// we use world-relative directions in the case of no main camera
+		m_Move = v*Vector3.forward + h*Vector3.right;
 		Move(m_Move);
 	}
 
 	public void Move(Vector3 move)
-	{
-		
+	{		
 		// convert the world relative moveInput vector into a local-relative
 		// turn amount and forward amount required to head in the desired
 		// direction.
@@ -104,38 +84,15 @@ public class PlayerControler : MonoBehaviour {
 			directionMove += new Vector3(1,0,0);
 			isKeyTouching = true;
 		}
-
-		//approach 1 move with transform//////////////////////////
-		/*directionMove.Normalize ();
-		
-		gameObject.transform.position += directionMove * speed;
-		*/
-		/////////////////////////////////////////////////////////
-
-
-		//approach 2 move with velocity/////////////////
+		//approach 2 move with velocity
 		directionMove.Normalize ();
-
 		gameObject.GetComponent<Rigidbody> ().velocity = directionMove * speed * 100;
-		////////////////////////////////////////////////
-		
-//		float turnSpeed = 10.0f;
-//		float targetAngle = Mathf.Atan2(directionMove.z, directionMove.x) * Mathf.Rad2Deg;
-//		transform.rotation = 
-//			Quaternion.Slerp( transform.rotation, 
-//			                 Quaternion.Euler( targetAngle, 0, targetAngle ), 
-//			                 turnSpeed * Time.deltaTime );
-
 		directionMove = new Vector3 (0, 0, 0);
 
 		return isKeyTouching;
 	}
 
-	void HandleAnimation(bool isKeyTouching){		
-//		float moveVerticle = Mathf.Abs(Input.GetAxis ("Vertical"));
-//		float moveHorizontal = Mathf.Abs(Input.GetAxis ("Horizontal"));
-//		float move = moveVerticle > moveHorizontal ? moveVerticle : moveHorizontal;
-//		Debug.Log (move);
+	void HandleAnimation(bool isKeyTouching){
 		float move = isKeyTouching ? 1 : 0;
 		_animator.SetFloat("Speed", move);
 		
