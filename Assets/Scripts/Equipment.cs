206242abd18ce1e02d2b4f792226d9c854e636e2
hdkhoa <huynhdangkhoa.itsoftware@gmail.com>
2015-06-19 11:15:27 +0700

    add weapon to ninja, quip by 'C' keyboard

---
+++ Assets/Scripts/Equipment.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Equipment : MonoBehaviour {
+
+	public GameObject _prefabWeapon = null;
+	public Transform _transform = null;
+	// Use this for initialization
+	private GameObject _weapon = null;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.C)) {
+			if(_weapon != null)
+				Destroy(_weapon);
+			_weapon = Instantiate (_prefabWeapon, _transform.position, _transform.rotation) as GameObject;
+			_weapon.transform.Rotate(0,180,0);
+			_weapon.transform.SetParent(_transform);
+		}
+	}
+}
