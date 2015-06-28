4f6e753a6db39a84f49aa1a0e1007be11174e58f
Duy <alvisstar1991@gmail.com>
2015-06-28 18:40:23 +0700

    thay res

--- Assets/Scripts/CharacterCollision.cs
+++ Assets/Scripts/CharacterCollision.cs
@@ -5,7 +5,7 @@ public class CharacterCollision : MonoBehaviour {
 	
 	void OnCollisionEnter (Collision col)
 	{
-		if (col.gameObject.name == "RandomItem" || col.gameObject.name == "RandomItem(Clone)") {
+		if (col.gameObject.name == "Item" || col.gameObject.name == "Item(Clone)") {
 			GameObject randomItemPrefab = col.gameObject.GetComponent<RandomItem> ().RandomItemz ();
 			if (randomItemPrefab.name == "SpeedUp") {
 				GameObject speedUp = Instantiate (randomItemPrefab);
