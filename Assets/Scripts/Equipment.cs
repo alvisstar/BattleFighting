681dab9afc7cfb46eb81fa1dad1c1ae8e4466cae
huynh dang khoa <huynhdangkhoastar@gmail.com>
2015-06-29 22:03:55 +0700

    rotate weapon when

--- Assets/Scripts/Equipment.cs
+++ Assets/Scripts/Equipment.cs
@@ -7,7 +7,7 @@ public class Equipment : MonoBehaviour {
 	public Transform _righthandTransform = null;
 	// Use this for initialization
 	public GameObject _weapon = null;
-
+	public Vector3 _initRotation = new Vector3(0,0,90);
 	void Start () {
 
 	}
@@ -17,7 +17,7 @@ public class Equipment : MonoBehaviour {
 		if(_weapon != null)
 			Destroy(_weapon);
 		_weapon = Instantiate (prefabWeapon, _righthandTransform.position, _righthandTransform.rotation) as GameObject;
-		_weapon.transform.Rotate(0,180,0);
+		_weapon.transform.Rotate(_initRotation);
 		_weapon.transform.SetParent(_righthandTransform);
 
 		Hashtable hash = new Hashtable();
