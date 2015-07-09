eba6224dccbb29d59047a7c19365a82e7527c59a
Tuan Dung <tuandung@Tuans-Mac-mini.local>
2015-07-09 16:34:20 +0700

    complete samurai skill 1.

--- Assets/Scripts/Skill/SamuraiSkill.cs
+++ Assets/Scripts/Skill/SamuraiSkill.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class SamuraiSkill : Skill {
 
+	private float currentTime = 0;
+	private bool isBeingActiveSkill1 = false;
+	private Vector3 startPos;
+	public float rangeOfSkill1 = 10;
+	public float timeOfSkill1 = 0.5f;
+
 	// Use this for initialization
 	void Start ()
 	{
@@ -11,18 +17,30 @@ public class SamuraiSkill : Skill {
 	
 	// Update is called once per frame
 	void Update () {
-		if (GetComponent<Rigidbody> ().velocity ==Vector3.zero ) {
-			_player.DisableTrail();
-			_player.setAllowControl(true);
-			_player._animator.SetBool ("IsSkill1",false);
-			_player._animator.SetBool ("IsSkill",false);
+		if (isBeingActiveSkill1) {
+			currentTime += Time.deltaTime;
 
+			if (Vector3.Distance (startPos, GetComponent<Rigidbody> ().position) >= rangeOfSkill1
+			    || currentTime >= timeOfSkill1) {
+				isBeingActiveSkill1 = false;
+
+				_player.DisableTrail ();
+				_player.setAllowControl (true);
+				_player._animator.SetBool ("IsSkill1", false);
+				_player._animator.SetBool ("IsSkill", false);
+
+				GetComponent<Rigidbody>().velocity = Vector3.zero;
+			}
+		} else {
+			currentTime = 0;
 		}
 	}
 
 	public override void activeSkill1 ()
 	{
 		base.activeSkill1 ();
+		startPos = GetComponent<Rigidbody> ().position;
+		isBeingActiveSkill1 = true;
 		GetComponent<Rigidbody>().AddForce(transform.forward*400,ForceMode.Impulse);
 		_player.ActiveTrail ();
 	}
